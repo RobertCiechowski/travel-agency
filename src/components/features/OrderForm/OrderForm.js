5324b3567d132ea643f7b680c2782b0fc4d81219
Robert Ciechowski <croberttm@gmail.com>
2020-08-20 21:00:15 +0200

    Add OrderOption component and it's subcomponents. (Show exact option type) (14.3)

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
 import PropTypes from 'prop-types';
+import OrderOption from '../OrderOption/OrderOption';
+import pricing from '../../../data/pricing.json';
 
 class OrderForm extends React.Component {
   render() {
     const { tripCost, options } = this.props;
     return (
       <Row>
+        {pricing.map(opt => (
+          <Col md={4} key={opt.id}>
+            <OrderOption {...opt} />
+          </Col>
+        ))}
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} options={options} />
         </Col>
