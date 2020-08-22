2b7121415fd66d83cbb5543d043e87da27084231
Robert Ciechowski <croberttm@gmail.com>
2020-08-23 00:49:54 +0200

    Change component OrderForm. (15.4 done)

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,32 +1,76 @@
 import React from 'react';
-import { Row, Col } from 'react-flexbox-grid';
-import OrderSummary from '../OrderSummary/OrderSummary.js';
 import PropTypes from 'prop-types';
+import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
+import Button from '../../common/Button/Button';
+import settings from '../../../data/settings';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {Row, Col} from 'react-flexbox-grid';
 
-class OrderForm extends React.Component {
-  render() {
-    const { tripCost, options, setOrderOption} = this.props;
-    return (
-      <Row>
-        {pricing.map(opt => (
-          <Col md={4} key={opt.id}>
-            <OrderOption {...opt} currentValue={options[opt.id]} setOrderOption={setOrderOption} />
-          </Col>
-        ))}
-        <Col xs={12}>
-          <OrderSummary tripCost={tripCost} options={options} />
-        </Col>
-      </Row>
-    );
+const sendOrder = (options, tripCost, tripName, tripId, tripCountry) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const {contact, name} = options;
+
+  if (name.length < 3) {
+    window.alert('Fill in correct name, please');
+    return;
   }
-}
+  if (contact.length < 7) {
+    window.alert('Fill in correct contact, please');
+    return;
+  }
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripCountry,
+    tripName,
+    tripId,    
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({tripCost, options, setOrderOption, tripName, tripId, tripCountry}) => (
+  <Row>
+    {pricing.map(option =>
+      <Col md={4} key={option.id}>
+        <OrderOption currentValue={options[option.id]} setOrderOption={setOrderOption} {...option}/>
+      </Col>
+    )}
+    <Col xs={12}>
+      <OrderSummary tripCost={tripCost} options={options} />
+      <Button onClick={() => sendOrder(options, tripCost, tripName, tripId, tripCountry, setOrderOption)}>Order now!</Button>
+    </Col>
+  </Row>
+);
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripId: PropTypes.string,
+  tripName: PropTypes.string,
+  tripCountry: PropTypes.object,
 };
 
-export default OrderForm; 
+export default OrderForm;
